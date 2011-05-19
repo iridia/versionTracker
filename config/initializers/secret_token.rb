e6fa0db691d3b013f74b3f2fa909e4c1e3b84648
Evadne Wu <ev@monoceroi.com>
2011-05-19 13:32:50 +0800

    rails 3.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IridiaVersionTracker::Application.config.secret_token = 'ad09c2d7da0a3064fb5605f61215cbdb199f79351147166e2f80f804b049d78fb0bacef496e2b28ebab45f76f0502ad5a2dc94a3b414e603f01c4d97ece3aa2e'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_new_session',
-  :secret      => '2d6dcbf02151f6fa2e36261968af5c7bbe2bb6e32df84090d721ad954e98045e9eedeba5f69e175d98cc17c458477ac72ead42a7b149c5f608d59258f05cc552'
-}
+IridiaVersionTracker::Application.config.session_store :cookie_store, :key => '_iridia-versionTracker_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# IridiaVersionTracker::Application.config.session_store :active_record_store
